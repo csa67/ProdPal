d66d687dbb0e3bc95d9579950868f84633ccbd97
csa67 <saiaarathy@gmail.com>
2024-03-22 20:25:21 -0400

    Adding buttons to control the animation

--- lib/boxBreathingAnimation.dart
+++ lib/boxBreathingAnimation.dart
@@ -51,10 +51,12 @@ class BreathingCircle extends StatefulWidget {
   _BreathingCircleState createState() => _BreathingCircleState();
 }
 
+
 class _BreathingCircleState extends State<BreathingCircle> with SingleTickerProviderStateMixin {
   late final AnimationController _controller;
   late final Animation<double> _positionAnimation;
   BreathingState state = BreathingState.breatheIn;
+  bool isStarted = false;
 
   @override
   void initState() {
@@ -70,21 +72,40 @@ class _BreathingCircleState extends State<BreathingCircle> with SingleTickerProv
     ))
       ..addListener(() {
         setState(() {
-          // This will trigger a rebuild with the new ball position.
+          final section = _positionAnimation.value;
+          if (section <= 1) {
+            state = BreathingState.breatheIn;
+          } else if (section <= 2) {
+            state = BreathingState.holdIn;
+          } else if (section <= 3) {
+            state = BreathingState.breatheOut;
+          } else {
+            state = BreathingState.holdOut;
+          }
         });
-        final section = _positionAnimation.value;
-        if (section <= 1) {
-          state = BreathingState.breatheIn;
-        } else if (section <= 2) {
-          state = BreathingState.holdIn;
-        } else if (section <= 3) {
-          state = BreathingState.breatheOut;
-        } else {
-          state = BreathingState.holdOut;
-        }
       });
 
-    _controller.repeat();
+    // Do not start the animation immediately
+    //_controller.repeat();
+  }
+
+  void _toggleStart() {
+    if (!isStarted) {
+      _controller.repeat();
+    } else {
+      _controller.stop();
+    }
+
+    setState(() {
+      isStarted = !isStarted;
+    });
+  }
+
+  void _resetAnimation() {
+    _controller.reset();
+    setState(() {
+      isStarted = false;
+    });
   }
 
   @override
@@ -116,13 +137,14 @@ class _BreathingCircleState extends State<BreathingCircle> with SingleTickerProv
         left = trackLength - (progress * trackLength);
         break;
       case 3: // Left side
-        top = trackLength - (progress * trackLength); // Adjust to align with the border
-        left = -ballDiameter / 2; // Adjust to align with the border
+        top = trackLength - (progress * trackLength); // Move up
+        left = -halfBallDiameter; // Stay at the left edge
         break;
     }
 
     return Offset(left + halfBallDiameter, top + halfBallDiameter);
   }
+
 
   @override
   Widget build(BuildContext context) {
@@ -130,54 +152,91 @@ class _BreathingCircleState extends State<BreathingCircle> with SingleTickerProv
     const double squareSize = 220.0;
     const innerCircleSize = 180.0;
     const double ballDiameter = 20.0;
+    String btnText = 'Pause';
 
     Offset ballPosition = calculateBallPosition(_positionAnimation.value, squareSize, ballDiameter);
 
-    return SizedBox(
-      width: outerSquareSize,
-      height: outerSquareSize,
-      child: Stack(
-        alignment: Alignment.center,
-        children: [
-          Container(
-            width: squareSize,
-            height: squareSize,
-            decoration: BoxDecoration(
-              color: Colors.transparent,
-              border: Border.all(color: Colors.white, width: 6),
-              borderRadius: BorderRadius.circular(20),
-            ),
-          ),
-          Container(
-            width: innerCircleSize, // Consider the border width for inner circle size
-            height: innerCircleSize,
-            decoration: const BoxDecoration(
-              shape: BoxShape.circle,
-              color: Colors.white,
-            ),
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        SizedBox(
+          width: outerSquareSize,
+          height: outerSquareSize,
+          child: Stack(
             alignment: Alignment.center,
-            child: Text(
-              state.label,
-              style: const TextStyle(
-                color: Colors.black,
-                fontSize: 24,
+            children: [
+              Container(
+                width: squareSize,
+                height: squareSize,
+                decoration: BoxDecoration(
+                  color: Colors.transparent,
+                  border: Border.all(color: Colors.white, width: 6),
+                  borderRadius: BorderRadius.circular(20),
+                ),
               ),
-            ),
-          ),
-          Positioned(
-            top: ballPosition.dy,
-            left: ballPosition.dx,
-            child: Container(
-              width: ballDiameter,
-              height: ballDiameter,
-              decoration: const BoxDecoration(
-                color: Colors.white, // Ball color is black
-                shape: BoxShape.circle,
+              Container(
+                width: innerCircleSize, // Consider the border width for inner circle size
+                height: innerCircleSize,
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.white,
+                ),
+                alignment: Alignment.center,
+                child: Text(
+                  state.label,
+                  style: const TextStyle(
+                    color: Colors.black,
+                    fontSize: 24,
+                  ),
+                ),
               ),
-            ),
+              Positioned(
+                top: ballPosition.dy,
+                left: ballPosition.dx,
+                child: Container(
+                  width: ballDiameter,
+                  height: ballDiameter,
+                  decoration: const BoxDecoration(
+                    color: Colors.white, // Ball color is black
+                    shape: BoxShape.circle,
+                  ),
+                ),
+              ),
+            ],
           ),
-        ],
-      ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: isStarted
+              ? Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  if (_controller.isAnimating) {
+                    _controller.stop();
+                    btnText = 'Resume';
+
+                  } else {
+                    _controller.forward();
+                    btnText = 'Pause';
+                  }
+                },
+                child: Text(btnText),
+              ),
+              SizedBox(width: 8),
+              ElevatedButton(
+                onPressed: _resetAnimation,
+                child: const Text('Reset'),
+              ),
+            ],
+          )
+              : ElevatedButton(
+            onPressed: _toggleStart,
+            child: const Text('Start'),
+          ),
+        ),
+      ],
     );
   }
 }
